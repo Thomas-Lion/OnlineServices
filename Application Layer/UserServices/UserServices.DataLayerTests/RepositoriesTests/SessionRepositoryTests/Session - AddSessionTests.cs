c8f2985ad02dbd5113d357d3533dfcd965201be6
TheRealDelep <41925548+TheRealDelep@users.noreply.github.com>
2020-02-21 15:58:06 +0100

    Checking for valid course in SessionRepo Add Test

--- Application Layer/UserServices/UserServices.DataLayerTests/RepositoriesTests/SessionRepositoryTests/Session - AddSessionTests.cs
+++ Application Layer/UserServices/UserServices.DataLayerTests/RepositoriesTests/SessionRepositoryTests/Session - AddSessionTests.cs
@@ -84,6 +84,63 @@ namespace RegistrationServices.DataLayerTests.RepositoriesTests.SessionRepositor
         }
 
         [TestMethod]
+        public void Should_Throw_Exception_When_Course_IsArchived()
+        {
+            var options = new DbContextOptionsBuilder<RegistrationContext>()
+                .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+                .Options;
+
+            using (var context = new RegistrationContext(options))
+            {
+                IRSUserRepository userRepository = new UserRepository(context);
+                IRSSessionRepository sessionRepository = new SessionRepository(context);
+                IRSCourseRepository courseRepository = new CourseRepository(context);
+
+                var Teacher = new UserTO()
+                {
+                    //Id = 420,
+                    Name = "Christian",
+                    Email = "gyssels@fartmail.com",
+                    Role = UserRole.Teacher
+                };
+
+                var Michou = new UserTO()
+                {
+                    //Id = 45,
+                    Name = "Michou Miraisin",
+                    Email = "michou@superbg.caca",
+                    Role = UserRole.Attendee
+                };
+
+                var AddedTeacher = userRepository.Add(Teacher);
+                var AddedAttendee = userRepository.Add(Michou);
+                context.SaveChanges();
+
+                var SQLCourse = new CourseTO()
+                {
+                    //Id = 28,
+                    Name = "SQL"
+                };
+
+                var AddedCourse = courseRepository.Add(SQLCourse);
+                context.SaveChanges();
+
+                var SQLSession = new SessionTO()
+                {
+                    //Id = 1,
+                    Attendees = new List<UserTO>()
+                        {
+                            AddedAttendee
+                        },
+                    Course = AddedCourse,
+                    Teacher = AddedTeacher,
+                };
+
+                Assert.ThrowsException<ArgumentException>(() => sessionRepository.Add(SQLSession));
+            }
+        }
+
+        [TestMethod]
         public void Should_Insert_Session_Without_Teacher_And_Users()
         {
             var options = new DbContextOptionsBuilder<RegistrationContext>()
